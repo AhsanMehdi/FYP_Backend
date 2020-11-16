94c70336fe265ecfd7d3ab2a9287976dfa6f9519
Ahsan Mehdi <mmahsanmehdi@gmail.com>
2020-11-16 09:40:47 +0500

    campaign added in type\express mongose

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,6 +3,7 @@ import { IUser } from '../../interfaces/IUser';
 import { INgoProfile } from '../../interfaces/INgoProfile';
 import { IDonorProfile } from '../../interfaces/IDonorProfile';
 import { IProject } from '../../interfaces/IProject';
+import { ICampaign } from '../../interfaces/ICampaign';
 
 
 declare global {
@@ -18,6 +19,8 @@ declare global {
     export type NgoProfileModel = Model<INgoProfile & Document>;
     export type DonorProfileModel = Model<IDonorProfile & Document>;
     export type ProjectModel = Model<IProject & Document>;
+    export type CampaignModel = Model<ICampaign & Document>;
+
 
   }
 }
