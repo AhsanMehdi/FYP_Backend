f1972e1bb677591e74646905a1c150ec3e38031c
Ahsan Mehdi <mmahsanmehdi@gmail.com>
2021-01-12 16:51:46 +0500

    in progress delete api

--- src/api/routes/profile.ts
+++ src/api/routes/profile.ts
@@ -208,6 +208,25 @@ route.put (
       },
     );
     
+    /* Custom API to delete donors by id */
+    // route.delete(
+    //   '/donor/:id',
+    //   async (req: Request, res: Response, next: NextFunction) => {
+    //     const logger:Logger = Container.get('logger');
+    //     logger.debug('Calling Donor endpoint with body: %o', req.body );
+    //     try {
+
+    //       var id = req.params.id;
+    //       const profileServiceInstance = Container.get(ProfileService);
+    //       const { donor} = await profileServiceInstance.DeleteDonorById(id);
+    //       return res.status(201).json({ donor });
+    //     } catch (e) {
+    //       logger.error('🔥 error: %o', e);
+    //       return next(e);
+    //     }
+    //   },
+    // );
+    
 
   /**
    * @TODO Let's leave this as a place holder for now
